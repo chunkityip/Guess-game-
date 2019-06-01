fe71624b416ea1bae2a52f26aa18455438bf53c0
chunkityip <47329780+chunkityip@users.noreply.github.com>
2019-06-01 14:09:12 -0400

    Add files via upload

---
+++ Guessing Game.py
@@ -0,0 +1,30 @@
+import random
+
+number = random.randint(1,101)
+
+guessthenumber = 0
+
+
+
+while True:
+    guessthenumber = int(input("Enter the number: "))
+
+    if guessthenumber < 1 or guessthenumber >100:
+        print("You can enter the number between 1 to 100 only!")
+
+    elif guessthenumber == number:
+        print("Bongo")
+
+
+
+    elif guessthenumber > 10:
+        print("Warm")
+        print(number)
+else:
+        print("Cold")
+        print(number)
+
+
+
+
+
