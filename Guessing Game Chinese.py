d7c1578987ece7f808277fe6d7b94a6061da4181
chunkityip <47329780+chunkityip@users.noreply.github.com>
2019-06-01 21:45:00 -0400

    Add files via upload

---
+++ Guessing Game Chinese.py
@@ -0,0 +1,24 @@
+import random
+
+number = random.randint(1,101)
+
+guessthenumber = 0
+
+
+
+while True:
+    guessthenumber = int(input("輸入數字："))
+
+    if guessthenumber < 1 or guessthenumber >100:
+        print("您只能輸入1到100之間的數字！")
+
+    elif guessthenumber == number:
+        print("得左")
+
+
+    elif guessthenumber > 10:
+        print("Warm, 數字大於10")
+        print(number)
+    elif guessthenumber < 10:
+        print("Cold,數字少於10")
+        print(number)
